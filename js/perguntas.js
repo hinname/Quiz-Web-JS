922fb8f7c357df28b8a07e735085e5580446f894
Herivelton Costa <herivelton.PRo@gmail.com>
2021-09-13 10:19:28 -0300

    quiz ver 1.0

---
+++ js/perguntas.js
@@ -0,0 +1,56 @@
+var perguntas = [
+    {
+        pergunta: "Qual é o nome do criador desse quiz?",
+        respostas: ["Sem nome", "João", "Herivelton", "Robert"],
+        resposta: "Herivelton",
+        respondida: false
+    },
+    {
+        pergunta: "Quantos planetas Terra cabem dentro do Sol?",
+        respostas: ["Um milhão", "Cem", "Seiscentos", "Cento e cinquenta", "Dois milhões"],
+        resposta: "Um milhão",
+        respondida: false
+    },
+    {
+        pergunta: "Qual é a melhor comida?",
+        respostas: ["Strogonoff", "Sushi", "Churrasco", "Pizza", "Lasanha"],
+        resposta: "Pizza",
+        respondida: false
+    },
+    {
+        pergunta: "De quem é a famosa frase “Penso, logo existo”?",
+        respostas: ["Platão", "Galileu Galilei", "Descartes", "Sócrates", "Francis Bacon"],
+        resposta: "Descartes",
+        respondida: false
+    },
+    {
+        pergunta: "Qual o nome do irmão de Dante, protagonista da série de games Devil May Cry?",
+        respostas: ["Félix", "Kratos", "Phillipe", "Vergil", "Nero"],
+        resposta: "Vergil",
+        respondida: false
+    },
+    {
+        pergunta: "Quais o menor e o maior país do mundo?",
+        respostas: ["Vaticano e Rússia", "Nauru e China", "Mônaco e Canadá", "Malta e Estados Unidos", "São Marino e Índia"],
+        resposta: "Vaticano e Rússia",
+        respondida: false
+    },
+    {
+      pergunta: "Este é um exemplo de palíndromo:",
+      respostas: ["Roma é amor.", "O rato roeu a roupa do rei de Roma.", "Quem tem boca vai à Roma.", "O que é, o que é? Feito para andar e não anda?", "Uni, duni, tê, Salamê, mingüê, Um sorvete colorê, O escolhido foi você!"],
+      resposta: "Roma é amor.",
+      respondida: false
+    },
+    {
+      pergunta: "Qual a unidade que mede a intensidade do som?",
+      respostas: ["Decibel", "Frequência", "Hertz", "Compasso", "Ruído"],
+      resposta: "Decibel",
+      respondida: false
+    },
+    {
+      pergunta: "1000 - 2 x 5 =",
+      respostas: ["990", "4990", "4998", "995", "998"],
+      resposta: "990",
+      respondida: false
+    }
+];
---
+++ js/script.js
@@ -0,0 +1,181 @@
+var configuracoes = {
+    iniciado: false,
+    tempo: 10,
+    pontos: 0,
+    quantidadePerguntas: 0,
+    maxQuantidadePerguntas: 5,
+    perguntaAtual: undefined
+}
+//Pergunta dps de respondida, sai do array
+var perguntasExcluidas = []
+
+function embaralhar(array) {
+
+  let indexAtual = array.length - 1
+  
+
+  for (indexAtual; indexAtual > 0; indexAtual--) {
+
+    let indexAleatorio = Math.floor(Math.random() * (indexAtual + 1))
+    let temp = array[indexAtual]
+    array[indexAtual] = array[indexAleatorio]
+    array[indexAleatorio] = temp
+  }
+
+  return array
+}
+
+function getPergunta(){
+
+    if(perguntas[0].respondida == true) {
+      perguntasExcluidas.push(perguntas[0])
+      perguntas.shift()
+    }
+
+
+    return perguntas[0];
+}
+
+
+function gerarPergunta(){
+
+    if(configuracoes.quantidadePerguntas >= configuracoes.maxQuantidadePerguntas){
+        // alert("Você fez: ", configuracoes.pontos, "Pontos");
+        // alert("Você fez: " + configuracoes.pontos);
+        // alert("Você fez: " + configuracoes.pontos + " Pontos");
+        // alert(`Você fez: ${configuracoes.pontos} Pontos`);
+        //alert(`Jogo finalizado, você acertou ${configuracoes.pontos} perguntas`);
+        configuracoes.iniciado = false;
+
+
+        document.querySelector(".quiz").style.display = "none"
+        document.querySelector(".reiniciar").style.visibility = "visible"
+
+        document.querySelector(".resultado h3").style.visibility = "visible"
+        document.querySelector(".resultado h3").innerHTML = `Jogo finalizado, você acertou ${configuracoes.pontos} perguntas`;
+
+        clearInterval(tempo);
+
+        //console.log(tempo)
+
+        return;
+    }
+
+    var retorno = getPergunta();
+
+    //console.log(retorno)
+
+    configuracoes.tempo = 10;
+    configuracoes.quantidadePerguntas++;
+
+    document.querySelector(".quiz .tempo h3").innerHTML = "10";
+
+    document.querySelector(".quiz .pergunta h2").innerHTML = retorno.pergunta;
+    
+    document.querySelector(".quiz .respostas").innerHTML = "";
+
+    retorno.respostas = embaralhar(retorno.respostas)
+
+    for(resposta of retorno.respostas){
+        document.querySelector(".quiz .respostas").innerHTML += `<button onclick="tentarResposta('${resposta}')">${resposta}</button>`;
+    }
+
+    retorno.respondida = true;
+
+    configuracoes.perguntaAtual = retorno;
+
+}
+
+
+var tempo = setInterval(function(){
+
+    console.log("Tempo rodando");
+
+    if(configuracoes.iniciado){
+        configuracoes.tempo--;
+        //console.log(configuracoes.tempo);
+
+        document.querySelector(".quiz .tempo h3").innerHTML = configuracoes.tempo;
+
+        if(configuracoes.tempo <= 0){
+            console.log("Nova pergunta!");
+            gerarPergunta();
+            return;
+        }
+    }
+
+}, 1000);
+
+function reiniciarTempo() {
+  tempo = setInterval(function(){
+
+    console.log("Tempo rodando");
+
+    if(configuracoes.iniciado){
+        configuracoes.tempo--;
+        //console.log(configuracoes.tempo);
+
+        document.querySelector(".quiz .tempo h3").innerHTML = configuracoes.tempo;
+
+        if(configuracoes.tempo <= 0){
+            console.log("Nova pergunta!");
+            gerarPergunta();
+            return;
+        }
+    }
+
+  }, 1000);
+}
+
+
+function tentarResposta(resposta){
+    if(!configuracoes.iniciado){
+        alert("Jogo já está finalizado, não dá pra jogar..");
+        return;
+    }
+
+    if(configuracoes.perguntaAtual.resposta == resposta){
+        configuracoes.pontos++;
+        alert("Você acertou!");
+    }
+    else {
+        alert("Você errou!");
+    }
+
+    gerarPergunta();
+}
+
+function reiniciarVar() {
+  configuracoes.pontos = 0
+  configuracoes.quantidadePerguntas = 0
+  configuracoes.perguntaAtual = undefined
+}
+
+
+function iniciar(){
+
+    if(configuracoes.iniciado){
+        alert("Jogo já está iniciado, não dá mais pra voltar!")
+        return;
+    }
+
+    if(perguntasExcluidas.length > 0) {
+      reiniciarVar()
+      reiniciarTempo()
+      for(pergunta of perguntasExcluidas) {
+        perguntas.push(pergunta)
+      }
+    }
+
+    embaralhar(perguntas)
+    
+    gerarPergunta();
+    
+    configuracoes.iniciado = true;
+
+
+    document.querySelector(".quiz").style.display = "block";
+    document.getElementById('start').style.display = "none";
+
+    document.querySelector(".reiniciar").style.visibility = "hidden"
+}
